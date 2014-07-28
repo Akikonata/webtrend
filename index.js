8bff5eaa35770b23de8d16b8ec4be13aac849011
Akikonata <66875692@qq.com>
2014-07-28 17:41:57 +0800

    added pages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 (function() {
   var SwiperCover, SwiperPages, SwiperEnd;
-  var colors = ['', '#2fb86c', '#2eb1b3', '#27a0c0', '#26a3c3', '#2063b6']; //色彩集合
+  var colors = ['', '#2fb86c', '#2eb1b3', '#27a0c0', '#26a3c3', '#2063b6', 'black', 'red']; //色彩集合
   var gennerrateCssStyle = function(idx, direction, colors) {
     var before;
     if (direction === 'prev') {
@@ -39,6 +39,7 @@
     resistance: '100%',
     onSlideChangeEnd: function(e) {
       if (e.activeIndex === 1) {
+        Charts.get('area').init();
         $('#cover').css({
           'zIndex': 0
         });
@@ -63,8 +64,7 @@
           });
         }
       }
-      //console.log(pages[idx]);
-      //$(pages[idx]).find('.title').addClass('title-animate');
+      $(pages[idx]).find('.title').addClass('title-animate');
     },
     onSlidePrev: function(e) {
       var idx = e.activeIndex;
@@ -79,6 +79,29 @@
         $('#end').show();
       }
       gennerrateCssStyle(idx, 'next', colors);
+      //初始化图表
+      switch (idx) {
+        case 1:
+          Charts.get('area').init();
+          break;
+        case 2:
+          Charts.get('donut').init();
+          break;
+        case 3:
+          var pie = Charts.get('pie');
+          pie.init('pie-1', [47, 73], '通信&<br>社交');
+          pie.init('pie-2', [33, 44], '娱乐');
+          pie.init('pie-3', [27, 11], '工具');
+          pie.init('pie-4', [17, 13], '浏览器<br>&搜索');
+          pie.init('pie-5', [19, 17], '其他');
+        case 4:
+          Charts.get('column').init();
+          break;
+        case 5:
+
+        default:
+          break;
+      }
     },
     speed: 1000
   });
--- js/charts/include.js
+++ js/charts/include.js
@@ -1,8 +1,8 @@
-document.write( '<script src="../js/charts/utils.js"></script>' );
-document.write( '<script src="../js/charts/charts.js"></script>' );
-document.write( '<script src="../js/charts/area.js"></script>' );
-document.write( '<script src="../js/charts/column.js"></script>' );
-document.write( '<script src="../js/charts/donut.js"></script>' );
-document.write( '<script src="../js/charts/pie.js"></script>' );
-document.write( '<script src="../js/charts/p-donut.js"></script>' );
-document.write( '<script src="../js/charts/bubble.js"></script>' );
+document.write('<script src="js/charts/utils.js"></script>');
+document.write('<script src="js/charts/charts.js"></script>');
+document.write('<script src="js/charts/area.js"></script>');
+document.write('<script src="js/charts/column.js"></script>');
+document.write('<script src="js/charts/donut.js"></script>');
+document.write('<script src="js/charts/pie.js"></script>');
+document.write('<script src="js/charts/p-donut.js"></script>');
+document.write('<script src="js/charts/bubble.js"></script>');
