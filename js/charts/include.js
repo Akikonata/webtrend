517b5afde3fb4a7c47ca1ab761fafcf5af17f755
Ronny <ronaldinhokj@gmail.com>
2014-07-29 16:05:24 +0800

    update

--- js/charts/include.js
+++ js/charts/include.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.write( '<script src="../js/charts/utils.js"></script>' );
 document.write( '<script src="../js/charts/charts.js"></script>' );
 document.write( '<script src="../js/charts/area.js"></script>' );
@@ -8,13 +7,3 @@ document.write( '<script src="../js/charts/pie.js"></script>' );
 document.write( '<script src="../js/charts/p-donut.js"></script>' );
 document.write( '<script src="../js/charts/bubble.js"></script>' );
 document.write( '<script src="../js/charts/round.js"></script>' );
-=======
-document.write('<script src="js/charts/utils.js"></script>');
-document.write('<script src="js/charts/charts.js"></script>');
-document.write('<script src="js/charts/area.js"></script>');
-document.write('<script src="js/charts/column.js"></script>');
-document.write('<script src="js/charts/donut.js"></script>');
-document.write('<script src="js/charts/pie.js"></script>');
-document.write('<script src="js/charts/p-donut.js"></script>');
-document.write('<script src="js/charts/bubble.js"></script>');
->>>>>>> 57864beec9ecab241cfc6c727b0adce13acf7c68
