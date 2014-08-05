ea9d3b2c92c45ba9b2dba9c51713f7c6e7fa0ad2
Akikonata <66875692@qq.com>
2014-08-05 17:15:25 +0800

    format

--- js/preload.js
+++ js/preload.js
@@ -1,44 +1,45 @@
 (function(start) {
 	var imgs = [
-			'img/1-bg.jpg',
-			'img/back-cover.jpg',
-			'img/book.png',
-			'img/donuts.png',
-			'img/logos.png',
-			'img/numbers.png',
-			'img/p-donut.png',
-			'img/pics1.png',
-			'img/pics2.png',
-			'img/pie-bg.png',
-			'img/share.png'
-		];
+		'img/1-bg.jpg',
+		'img/back-cover.jpg',
+		'img/book.png',
+		'img/donuts.png',
+		'img/logos.png',
+		'img/numbers.png',
+		'img/p-donut.png',
+		'img/pics1.png',
+		'img/pics2.png',
+		'img/pie-bg.png',
+		'img/share.png'
+	];
 
 	var progress = '0%',
 		loaded = 0,
 		l = imgs.length,
-		hasStarted = false, 
+		hasStarted = false,
 		time = 1000;
 
-	function finish(p){
-		if(p == '100%' && hasStarted == false){
+	function finish(p) {
+		if (p == '100%' && hasStarted == false) {
 			hasStarted = true;
 			go();
 		}
 	}
 
-	function setProgress( p ){
+	function setProgress(p) {
 		// console.log( p );
 	}
 
-	function go(){
+	function go() {
+
 		start();
 	}
 
-	imgs.forEach(function( url, i ){
+	imgs.forEach(function(url, i) {
 		var pic = new Image();
-		pic.onload = function(){
+		pic.onload = function() {
 			loaded++;
-			progress = (loaded/l*100).toFixed(0) + '%';
+			progress = (loaded / l * 100).toFixed(0) + '%';
 
 			finish(progress);
 			setProgress(progress);
@@ -46,7 +47,7 @@
 		pic.src = url;
 	});
 
-	setTimeout(function(){
+	setTimeout(function() {
 		finish('100%');
 	}, time);
 
