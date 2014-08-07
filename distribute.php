83477649a43910a7bf07cb01e54d9e6f6460fe45
Ronny <ronaldinhokj@gmail.com>
2014-08-07 09:53:49 +0800

    update

--- distribute.php
+++ distribute.php
@@ -2,9 +2,19 @@
     require_once 'php/detect/Mobile_Detect.php';
     $detect = new Mobile_Detect;
 
+    $pdfLink = 'http://www.baidu.com/';
+// var_dump( floatval(substr($detect->version('Android'), 0, 3)) );
     if( $detect->isMobile() ){
-        include('index.html');
+
+    	if( $detect->isiOS() ){
+    		include('max.html');
+    	}else if( $detect->isAndroidOS() && floatval(substr($detect->version('Android'), 0, 3)) >= 4 ){
+    		include('max.html');
+    	}else{
+    		include('min.html');
+    	}
+        
     }else{
-        include('min.html');
+        header("Location: " . $pdfLink);
     }
 ?>
