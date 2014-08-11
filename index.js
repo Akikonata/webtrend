134b2e8de9ba76007585951accb695026cadd057
Ronny <ronaldinhokj@gmail.com>
2014-08-11 10:50:54 +0800

    update

--- index.js
+++ index.js
@@ -286,4 +286,9 @@ function start() {
     });
   }
 
+  $('#pdf-link').hammer().on('tap', function(){
+    var link = 'http://developer.baidu.com/static/assets/reportpdf/%E7%99%BE%E5%BA%A6%E7%A7%BB%E5%8A%A8%E8%B6%8B%E5%8A%BF%E6%8A%A5%E5%91%8A2014Q2.pdf';
+    window.open(link, '_blank');
+  });
+
 };
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     require_once 'php/detect/Mobile_Detect.php';
     $detect = new Mobile_Detect;
 
-    $pdfLink = 'http://www.baidu.com/';
-// var_dump( floatval(substr($detect->version('Android'), 0, 3)) );
+    $pdfLink = 'http://developer.baidu.com/static/assets/reportpdf/%E7%99%BE%E5%BA%A6%E7%A7%BB%E5%8A%A8%E8%B6%8B%E5%8A%BF%E6%8A%A5%E5%91%8A2014Q2.pdf';
+
     if( $detect->isMobile() ){
 
     	if( $detect->isiOS() ){
